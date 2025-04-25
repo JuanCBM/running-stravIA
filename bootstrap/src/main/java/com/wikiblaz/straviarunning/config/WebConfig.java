795a0a2e295641285f0086b305543bd433fce110
juablaz <jc-blazquez@hotmail.com>
2025-04-25 17:03:27 +0200

    Up CORS

--- bootstrap/src/main/java/com/wikiblaz/straviarunning/config/WebConfig.java
+++ bootstrap/src/main/java/com/wikiblaz/straviarunning/config/WebConfig.java
@@ -24,7 +24,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(frontendUrl, "https://juancbm.github.io")
+                .allowedOrigins(frontendUrl, "https://juancbm.github.io", "https://juancbm.koyeb.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
